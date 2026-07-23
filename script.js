fee71b2a1b1f53b139c8689ed5998d6aebbf65cf
gourob-id <unnatibiswas4252@gmail.com>
2026-07-23 10:03:15 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+console.log("Welcome to Gourob Tools - ID Print Pro");
+
+document.querySelectorAll("button").forEach(button=>{
+    button.addEventListener("click",()=>{
+        alert("এই ফিচারটি শীঘ্রই যোগ করা হবে।");
+    });
+});
